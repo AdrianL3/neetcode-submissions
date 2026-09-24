a28af4d805f8cb1ffa11b96f2d4e3051accae85f
Adrian Laksana <146301373+AdrianL3@users.noreply.github.com>
2026-09-23 21:59:03 -0700

    Add: merge-sorted-array - submission-0

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        # 3 pointer technique that fills backwards
+        last = m + n - 1
+
+        while n > 0 and m > 0:
+            upper = nums1[m - 1]
+            lower = nums2[n - 1]
+
+            if lower >= upper:
+                nums1[last] = nums2[n - 1]
+                n -= 1
+            else:
+                nums1[last] = nums1[m - 1]
+                m -= 1
+            last -= 1
+
+        # case when ms are done before n
+        while n > 0:
+            nums1[last] = nums2[n - 1]
+            n -= 1
+            last -= 1
